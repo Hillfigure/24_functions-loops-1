68750516d4bcb5d731b42359669cd3f40ea79288
Hillfigure <49096115+Hillfigure@users.noreply.github.com>
2021-10-17 12:19:51 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+function paintWall(wall, wallColor) {
+    console.log("The " + wall + "wall has been painted " + wallColor + ".");;
+}
+
+paintWall("north wall", "red");
+
+paintWall("south wall", "green");
